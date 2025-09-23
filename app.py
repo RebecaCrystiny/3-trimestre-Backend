0b10934cf5ab63ade3570fb1ca9c6913ef91319e
RebecaCrystiny <161484475+RebecaCrystiny@users.noreply.github.com>
2025-09-23 08:50:37 -0300

    23/09

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ app = Flask(__name__)
 def hello_world():
      return render_template ('index.html')
 
-#executa o servidor de desenvolvimento
-if__name__ == '__main__':
-    app.run(debug=True)
+@app.route ('/login', methods=['GET', 'POST'])
+def login():
+     error=None
+     if request.method == 'POST':
+          username = request.form ['username']
+          password = request.form ['password']
+          if username == 'admin' and password == 'password':
+               return 'Login com sucesso' 
+          else:
+               error = 'Credenciais invalidas. Tente novamente.'
+     return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+     port = int(os.environ.get('PORT', 5000))
+     app.run(host='0.0.0.0', port=port, debug=True)
