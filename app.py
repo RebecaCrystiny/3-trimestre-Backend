d41fcf2adae545a8ac78ec1921dd2a9523a49090
RebecaCrystiny <161484475+RebecaCrystiny@users.noreply.github.com>
2025-10-01 09:32:57 -0300

    01/10

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-#rodar no cmd
-#pip install Flask
 
-#importar biblioteca Flask
 from flask import Flask
-#Cria uma instancia de aplicação Flask
+
 app = Flask(__name__)
-#este é um decorador que associa a URL
-# '/' (a URL é do site) á função que vem logo abaixo
+
 @app.route('/')
-#a função que é executada quando a rota '/' é acessada 
-#ela retorna a string "hello world"
+
 def hello_world():
      return render_template ('index.html')
 
